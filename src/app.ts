27c976a19577e9648a1f9ab8597425728fb9e88b
sjparker94 <simon.parker@hotmail.com>
2025-04-21 13:48:26 +0100

    feat: add users router to main app routes

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,13 @@ import { createApp } from '~/lib/create-app';
 import { configureOpenAPI } from '~/lib/open-api';
 
 import { indexRouter } from './routes/index.route';
+import { usersRouter } from './routes/users/users.index';
 
 const app = createApp();
 
 configureOpenAPI(app);
 
-const routes = [
-    indexRouter,
-    // add user routes here
-] as const;
+const routes = [indexRouter, usersRouter] as const;
 
 for (const route of routes) {
     app.route('/', route);
