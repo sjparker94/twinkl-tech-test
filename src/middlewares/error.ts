e4f38b853e7bae15a641f29569817a98e08bd37c
sjparker94 <simon.parker@hotmail.com>
2025-04-22 07:55:28 +0100

    feat: update error handling middleware to return the standardised response format

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -2,31 +2,38 @@ import type { ErrorHandler, NotFoundHandler } from 'hono';
 
 import type { StatusCode } from '~/constants/http';
 
+import { apiEndpointStatuses } from '~/constants/api';
 import { HTTP_STATUS_VALUES } from '~/constants/http';
 import { env } from '~/env';
 
 export const onError: ErrorHandler = (err, c) => {
-    const currentStatus
-        = 'status' in err ? err.status : c.newResponse(null).status;
-    const statusCode
-        = currentStatus !== 200
+    const currentStatus =
+        'status' in err ? err.status : c.newResponse(null).status;
+    const statusCode =
+        currentStatus !== 200
             ? (currentStatus as StatusCode)
             : HTTP_STATUS_VALUES.INTERNAL_SERVER_ERROR.code;
 
-    return c.json(
-        {
+    return c.json({
+        status: apiEndpointStatuses.error,
+        error: {
             message: err.message,
             stack: env.NODE_ENV === 'production' ? undefined : err.stack,
+            statusCode,
         },
         statusCode,
-    );
+    });
 };
 
 export const notFound: NotFoundHandler = (c) => {
     return c.json(
         {
-            message: `${HTTP_STATUS_VALUES.NOT_FOUND.message} - ${c.req.path}`,
-            statusCode: HTTP_STATUS_VALUES.NOT_FOUND.code,
+            status: apiEndpointStatuses.error,
+            error: {
+                message: `${HTTP_STATUS_VALUES.NOT_FOUND.message} - ${c.req.path}`,
+
+                statusCode: HTTP_STATUS_VALUES.NOT_FOUND.code,
+            },
         },
         HTTP_STATUS_VALUES.NOT_FOUND.code,
     );
